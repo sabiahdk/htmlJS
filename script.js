16a8e32560f5edb43efc2987df46045a50ebe4bd
sabiahdk <sabiahmuhd98@gmail.com>
2025-05-22 16:54:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// console.log('welcome to sail')
+
+// console.log(document.getElementsByClassName('text'))
+//  document.getElementById
+//  document.querySelector
+//  document.querySelectorAll
+
+
+
+
+// console.log(document.getElementsByClassName('class'))
+// console.log(document.querySelectorAll('p'))
+
+// textContext
+// innerHTML
+// innerText
+
+
+
+
+// let student = {name :'sabiah',
+//     nationality: 'nigeria',
+//     status: 'second to the last born' 
+
+// }
+
+// innerText;'',
+// console.log(document.getElementById('').innerText)
+
+// console.log(document.getElementsByClassName('list'))
+
+console.log(document.querySelector('li'))
+console.log(window.document.querySelectorAll('li')[3].innerText)
+
+console.log(document.getElementsByClassName('li').textContent = 'garri')
